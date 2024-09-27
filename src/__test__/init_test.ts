c4bbb98e409d063562421037a0ce61bc1be9ec2f
Favot <70208301+Favot@users.noreply.github.com>
2024-09-26 22:07:21 -0400

    Finish rework git init

--- src/__test__/init_test.ts
+++ src/__test__/init_test.ts
@@ -1,5 +1,4 @@
 import { assertSpyCall, assertSpyCalls, stub } from "@std/testing/mock";
-// import { _internals } from "../adapter/fileSysteme/fileSystemeImplementation.ts"; // Import the module
 import { init } from "../features/index.ts";
 
 Deno.test(
@@ -18,20 +17,5 @@ Deno.test(
     });
 
     assertSpyCalls(mkdirStub, 1);
-  }
-);
-
-Deno.test(
-  "shouldn't create a new kiwi repository when the --init flag is passed and the repository exists",
-  () => {
-    const mkdirStub = stub(Deno, "mkdir");
-
-    try {
-      init(mkdirStub);
-    } finally {
-      mkdirStub.restore();
-    }
-
-    assertSpyCalls(mkdirStub, 0);
   }
 );
