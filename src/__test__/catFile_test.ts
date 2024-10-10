53148602f1742b4de2898a7a7c35ae2527abf8d6
Favot <70208301+Favot@users.noreply.github.com>
2024-10-10 22:19:56 +0200

    Fix catFile feature test

--- src/__test__/catFile_test.ts
+++ src/__test__/catFile_test.ts
@@ -1,17 +1,22 @@
 import { assertSpyCall, assertSpyCalls, spy } from "@std/testing/mock";
 import {
-    DEFAULT_FILE_SYSTEME_VALUE,
     MockFileSystemService,
 } from "../adapter/fileSystem/MockFileSystemService.ts";
+import { OBJECTS_DIR_PATH } from "../constants.ts";
 import { main } from "../main.ts";
 import { RealFeaturesService } from "../service/features/RealFeatureService.ts";
 import { MockHashService } from "../service/hash/MockHashService.ts";
 
 Deno.test(
-    "should call the catFile function when the --cat-file flag is passed with a file path",
+    "should call the catFile function when the --cat-file flag is passed with a object id",
     async () => {
+        const objectId = "0e08b5e8c10abc3e455b75286ba4a1fbd56e18a5";
         const mockFileSystem = new MockFileSystemService();
         mockFileSystem.setDefaultFileContent();
+        mockFileSystem.setFile(
+            `${OBJECTS_DIR_PATH}/0e08b5e8c10abc3e455b75286ba4a1fbd56e18a5`,
+            "test content",
+        );
 
         const mockHashService = new MockHashService();
         mockHashService.setDefaultHashResult();
@@ -20,7 +25,7 @@ Deno.test(
         using catFile = spy(featuresService, "catFile");
 
         await main({
-            inputArgs: ["--catFile=test.txt"],
+            inputArgs: [`--catFile=${objectId}`],
             fileSystemService: mockFileSystem,
             hashService: mockHashService,
             featureService: featuresService,
@@ -29,8 +34,7 @@ Deno.test(
         assertSpyCalls(catFile, 1);
         assertSpyCall(catFile, 0, {
             args: [
-                DEFAULT_FILE_SYSTEME_VALUE.fileName,
-                mockHashService,
+                objectId,
                 mockFileSystem,
             ],
         });
@@ -38,7 +42,7 @@ Deno.test(
 );
 
 Deno.test(
-    "should not call the catFilefunction and should log an error when the --cat-file flag is passed without a file path",
+    "should not call the catFilefunction and should log an error when the --cat-file flag is passed without a object id",
     () => {
         const featuresService = new RealFeaturesService();
         using catFile = spy(featuresService, "catFile");
