359d531f386b95f6ae4bbc85b3f945c9614661b8
Favot <70208301+Favot@users.noreply.github.com>
2024-10-30 22:31:44 +0100

    Implement readTree function

---
+++ src/service/features/readTree/readTree.ts
@@ -0,0 +1,30 @@
+import type { FileSystemService } from "../../../adapter/fileSystem/FileSystemService.ts";
+import type { FeaturesService } from "../FeaturesService.ts";
+import { getTree } from "./getTree.ts";
+
+export const readTree = async (
+    fileSystem: FileSystemService,
+    featuresService: FeaturesService,
+    objectId: string,
+    workingDirectory: string,
+) => {
+    const tree = await getTree(
+        fileSystem,
+        featuresService,
+        objectId,
+        workingDirectory,
+    );
+
+    for (const [filePath, objectId] of Object.entries(tree)) {
+        const filePathInWorkingDirectory = `${filePath}`;
+        const fileContent = await featuresService.getObject(
+            objectId,
+            fileSystem,
+            "blob",
+        );
+        await fileSystem.writeFile(
+            filePathInWorkingDirectory,
+            new TextEncoder().encode(fileContent),
+        );
+    }
+};
